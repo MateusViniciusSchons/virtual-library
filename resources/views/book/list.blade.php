587ad690cb91163613166054231432d49dfaed81
Mateus Vinicius Schons <mateusvschons@gmail.com>
2021-07-21 13:44:31 -0300

    adicionando cards bootstrap

--- resources/views/book/list.blade.php
+++ resources/views/book/list.blade.php
@@ -2,7 +2,19 @@
 @section('content')
 
     @foreach ($books as $book) 
-        <p> Livro: {{$book->title}}</p>
-        @endforeach
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">{{$book->title}}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{{$book->author}}</h6>
+                <p class="card-text">Publicado por {{$book->publishing_company}}</p>
+                <p class="card-text">Possui {{$book->pages_count}} páginas</p>
+                <a href="#" class="card-link">Ver mais</a>
+                <a href="#" class="card-link">Alterar</a>
+                <a href="#" class="card-link">Deletar</a>
+            </div>
+        </div>
+    @endforeach
+    <div class="pagination-controls">
         {{$books->links()}}
+    </div>
 @stop
