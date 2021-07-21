181a8ee23804eac3449c12421820b34c3842af94
Mateus Vinicius Schons <mateusvschons@gmail.com>
2021-07-21 09:12:04 -0300

    Criando a tabela "books" com migrations

---
+++ database/migrations/2021_07_21_115926_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+
+            $table->id();
+            $table->string("title");
+            $table->string("publishing_company", 150);
+            $table->string("author", 100);
+            $table->integer("pages_count");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
