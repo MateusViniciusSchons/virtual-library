06e2f4a9698c7648a198c8b0536b5312e96cd0c3
Mateus Vinicius Schons <mateusvschons@gmail.com>
2021-07-21 15:02:44 -0300

    Adicionando Fonte Roboto e estilos à lista

--- resources/views/book/list.blade.php
+++ resources/views/book/list.blade.php
@@ -1,20 +1,31 @@
 @extends('base/head')
 @section('content')
+    <section class="page-content">
+        <h1 class="title">Livros da Biblioteca</h1>
 
-    @foreach ($books as $book) 
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">{{$book->title}}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">{{$book->author}}</h6>
-                <p class="card-text">Publicado por {{$book->publishing_company}}</p>
-                <p class="card-text">Possui {{$book->pages_count}} páginas</p>
-                <a href="#" class="card-link">Ver mais</a>
-                <a href="#" class="card-link">Alterar</a>
-                <a href="#" class="card-link">Deletar</a>
-            </div>
+        <div class="books">
+            @foreach ($books as $book) 
+                <div class="card book-card-body" style="width: 90%;">
+                    <div class="card-body book-card-body">
+                        <h5 class="card-title book-title">{{$book->title}}</h5>
+                        <h6 class="card-subtitle book-author mb-2 text-muted">Autor(a): {{$book->author}}</h6>
+                        <p class="card-text book-company">Editora: {{$book->publishing_company}}.</p>
+                        <p class="card-text book-pages-count">Possui {{$book->pages_count}} páginas.</p>
+                        <div class="links-section">
+                            <a href="#" class="card-link see-more-button">Ver mais</a>
+                            <div class="options-group">
+                                <a href="#" class="card-link change-button">Alterar</a>
+                                <a href="#" class="card-link delete-button">Deletar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
-    @endforeach
-    <div class="pagination-controls">
-        {{$books->links()}}
-    </div>
+        <div class="pagination-controls">
+            {{$books->links()}}
+        </div>
+
+    </section>
+    
 @stop
