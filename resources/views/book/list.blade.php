8e0818c5080c130fc52ad48a375dd481622b8668
Mateus Vinicius Schons <mateusvschons@gmail.com>
2021-07-22 17:19:29 -0300

    Tirando título para abrir o modal, agora usa slug

--- resources/views/book/list.blade.php
+++ resources/views/book/list.blade.php
@@ -27,7 +27,7 @@
                                     </a>
 
                                     <!-- Modal -->
-                                    <div class="modal fade" id="modal-{{$book->title}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal fade" id="modal-{{$book->slug}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                     <div class="modal-dialog">
                                         <div class="modal-content">
                                         <div class="modal-header">
